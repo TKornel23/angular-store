761608db24ebc9ed1ad9f1f15d153656dccca7db
Kornél Tóth <tkornel23@gmail.com>
2023-08-30 18:19:36 +0200

    menu done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -17,10 +17,14 @@ import { MatSnackBarModule } from '@angular/material/snack-bar';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HeaderComponent } from './components/header/header.component';
+import { HomeComponent } from './pages/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
